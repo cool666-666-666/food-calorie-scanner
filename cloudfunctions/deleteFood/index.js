a6b4f61e27d2d09d3173bf4ade3597bae652f5cb
cool-666-666-666 <liangshuangcool666@gmail.com>
2026-05-30 21:23:46 +0800

    auto: $(date +%Y-%m-%d %H:%M)

---
+++ cloudfunctions/deleteFood/index.js
@@ -0,0 +1,12 @@
+const cloud = require("wx-server-sdk");
+cloud.init({ env: cloud.DYNAMIC_CURRENT_ENV });
+const db = cloud.database();
+
+exports.main = async () => {
+  try {
+    const res = await db.collection("foods").where({ name: "欧包" }).remove();
+    return { success: true, removed: res.stats.removed };
+  } catch (e) {
+    return { success: false, errMsg: e.message };
+  }
+};
